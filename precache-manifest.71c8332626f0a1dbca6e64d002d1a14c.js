df9e290385fff468ca372439615c2d07834ab12d
NoobSaiyan <ms19961998@gmail.com>
2020-04-25 15:36:37 +0530

    Updates

---
+++ precache-manifest.71c8332626f0a1dbca6e64d002d1a14c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "316959e532915223aed20b4c2cc622c5",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "47cf1ba616a7d6247900",
+    "url": "/todo-app/static/css/main.34682927.chunk.css"
+  },
+  {
+    "revision": "ef6264b983b69961a06b",
+    "url": "/todo-app/static/js/2.73bac195.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-app/static/js/2.73bac195.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47cf1ba616a7d6247900",
+    "url": "/todo-app/static/js/main.83d3e239.chunk.js"
+  },
+  {
+    "revision": "8c574afb4aca7e7fd22e",
+    "url": "/todo-app/static/js/runtime-main.e86c861d.js"
+  }
+]);
