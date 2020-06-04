974128e0639f7dd97bbcc85cad85013871326945
NoobSaiyan <ms19961998@gmail.com>
2020-06-04 11:52:53 +0530

    Updating CSS for mobile use

--- src/components/result-card/result-card.component.jsx
+++ src/components/result-card/result-card.component.jsx
@@ -1,29 +1,26 @@
-import React from 'react'
-import Editable from '../editable-text/editable-text.component'
-import './result-card.style.css'
+import React from "react";
+import Editable from "../editable-text/editable-text.component";
+import "./result-card.style.css";
 
-export const Card = props =>{
-   
+export const Card = (props) => {
+  const handleDelete = () => {
+    setTimeout(props.delete(props.result), 300);
+  };
+  console.log("inside resultcard - " + props.id);
+  return (
+    <div className="item">
+      <label className="checkbox-label">
+        <input type="checkbox" />
+        <span className="checkbox-custom rectangular"></span>
+      </label>
 
-    const handleDelete = () => {
-        setTimeout(props.delete(props.result), 300)
-    }
-    console.log("inside resultcard - "+props.id)
-    return(
-        <div  className="item">
-            <label className="checkbox-label">
-                <input type="checkbox"/>
-                <span className="checkbox-custom rectangular"></span>
-            </label>
+      <Editable result={props.result} editItem={props.editItem} id={props.id} />
 
-            <Editable result={props.result} editItem={props.editItem} id={props.id}/>               
+      <div className="delete" onClick={handleDelete}>
+        X
+      </div>
+    </div>
+  );
+};
 
-            <div className="delete" onClick={handleDelete}>
-                X
-            </div>
-            
-        </div>
-    )
-}
-
-export default Card
+export default Card;
